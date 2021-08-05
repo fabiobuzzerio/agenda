4042d52ebc1e0d4c6d5993299ceeabe6d5797d8e
fabiobuzzerio <fabio2000b@gmail.com>
2021-08-05 20:06:27 +0200

    saveList compleatato

--- app.js
+++ app.js
@@ -1,75 +1,187 @@
-// TO DO LIST
-function focusNextTask(task) {
-	task.nextSibling.querySelector('span').focus()
+// LOCALSTORAGE OPTIONS & SETTINGS CHECKS
+let options = {
+  'theme': 'auto',
+  'moveCompletedTasks': true,
+  'saveList': true
 }
 
-function focusPreviousTask(task) {
-	task.previousSibling.querySelector('span').focus()
+if ('options' in localStorage) {
+  options = JSON.parse(localStorage.options)
+} else {
+  localStorage.options = JSON.stringify({
+    'theme': options.theme,
+    'moveCompletedTasks': options.moveCompletedTasks,
+    'saveList': options.saveList
+  })
+}
+
+document.querySelector(`input[name="theme"][value="${options.theme}"]`).checked = true
+document.querySelector('input[name="moveCompletedTasks"]').checked = options.moveCompletedTasks
+document.querySelector('input[name="saveList"]').checked = options.saveList
+
+function updateLSOptions() {
+  localStorage.options = JSON.stringify(options)
+}
+
+// DOM ELEMENTS
+list = document.getElementById('list')
+
+
+// OPEN & CLOSE MODALS
+var modals = [
+  document.getElementById('info'),
+  document.getElementById('settings')
+]
+var modalsToggles = [
+  document.getElementById('infoToggle'),
+  document.getElementById('settingsToggle')
+]
+
+function manageModal(modal, state) {
+  modal.classList.toggle('active', state)
+}
+
+modalsToggles.forEach((toggle, i) => {
+  toggle.addEventListener('click', () => {
+    manageModal(modals[i], true)
+  })
+})
+
+window.addEventListener('click', e => {
+  modals.forEach(modal => {
+    if (e.target == modal && modal.classList.contains('active')) manageModal(modal, false)
+  })
+})
+
+
+// DARK OR LIGHT MODE
+function manageTheme(a) {
+  document.body.classList.toggle('dark-theme', (a == 'dark' || (a == 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches)))
+}
+
+manageTheme(options.theme)
+
+document.querySelectorAll('input[name="theme"]').forEach(radio => {
+  radio.addEventListener('change', () => {
+    options.theme = radio.value
+    manageTheme(options.theme)
+    updateLSOptions()
+  })
+})
+
+window.matchMedia('(prefers-color-scheme: dark)').addListener(() => {
+  manageTheme(options.theme)
+})
+
+
+// MOVE COMPLETED TASKS
+document.querySelector('input[name="moveCompletedTasks"]').addEventListener('change', e => {
+  if (e.target.checked) {
+    lastTask = tasks[tasks.length-1]
+    document.querySelectorAll('.completed').forEach(task => {
+      insertAfter(task, lastTask)
+      lastTask = task
+    })
+    options.moveCompletedTasks = true
+  } else {
+    options.moveCompletedTasks = false
+  }
+  listSaver()
+  updateLSOptions()
+})
+
+
+// SAVE LIST
+function listSaver() {
+  if (!options.saveList) return
+  let tasksArray = []
+  document.querySelectorAll('.task').forEach(task => {
+    tasksArray.push({'text': task.querySelector('span').innerHTML, 'completed': (task.classList.contains('completed') ? true : false)})
+  })
+  localStorage.savedTasks = JSON.stringify(tasksArray)
+}
+
+document.querySelector('input[name="saveList"]').addEventListener('change', e => {
+  options.saveList = e.target.checked ? true : false
+  updateLSOptions()
+  if (!options.saveList) localStorage.savedTasks = []
+})
+
+
+// TO DO LIST
+function appendTask(node, position, completed, text) {
+  newTask = document.createElement('div')
+  newTask.classList = (completed ? 'task completed' : 'task')
+  newTask.innerHTML = `
+    <input type="checkbox">
+    <span contenteditable="${completed ? false : true}" placeholder="Type something...">${text}</span>
+  `
+  if (completed) newTask.querySelector('input').checked = true
+  node.insertAdjacentElement(position, newTask)
+}
+
+function focusTask(task, position) {
+  if (position == 'prev') task.previousSibling.querySelector('span').focus()
+  if (position == 'next') task.nextSibling.querySelector('span').focus()
 }
 
 function insertAfter(newNode, referenceNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
 }
 
-function createTask(prevTask) {
-	newTask = document.createElement('div')
-	newTask.classList = 'task'
-	newTask.innerHTML = `
-		<input type="checkbox">
-		<span contenteditable="true" placeholder="Type something..."></span>
-	`
-	insertAfter(newTask, prevTask)
-	newTask.querySelector('span').focus()
+if (options.saveList) {
+  tasks = JSON.parse(localStorage.savedTasks)
+  tasks.forEach(task => {
+    appendTask(list, 'beforeend', task['completed'], task['text'],)
+  })
+} else {
+  appendTask(list, 'afterbegin', false, '')
+  list.lastChild.querySelector('span').focus()
 }
+
+// document.body.querySelector('#list > :not(.completed)').lastChild.querySelector('span').focus()
+// list.querySelectorAll(':not(.completed)')
 
 window.addEventListener('keydown', e => {
 	if (e.key == 'Enter' || e.key == 'Tab') {
 		e.preventDefault()
 	}
-	if (e.target.parentNode.classList == 'task') {
+  if (e.ctrlKey && e.key == 's') {
+    e.preventDefault()
+		listSaver()
+	}
+	if (e.target.parentNode.classList.contains('task')) {
 		span = e.target
     task = span.parentNode
 		list = task.parentNode
 		if (e.key == 'Backspace' && span.innerHTML == '' && task !== list.querySelector('div > div')) {
-			focusPreviousTask(task)
-			list.removeChild(span.parentNode)
+			focusTask(task, 'prev')
+			list.removeChild(task)
 		}
 		if (e.key == 'Enter' && span.innerHTML !== '') {
-			createTask(task)
+			appendTask(task, 'afterend', false, '')
+      focusTask(task, 'next')
 		}
 		if (e.key == 'ArrowUp') {
-			focusPreviousTask(task)
+			focusTask(task, 'prev')
 		}
-		if (e.ctrlKey && e.key == 'ArrowUp') {
-			list.insertBefore(task, task.previousSibling)
-			span.focus()
-		}
-		if (e.key == 'ArrowDown') {
-			focusNextTask(task)
-		}
-		if (e.ctrlKey && e.key == 'ArrowDown') {
-			insertAfter(task, task.nextSibling)
-			span.focus()
-		}
+    if (e.key == 'ArrowDown') {
+      focusTask(task, 'next')
+    }
+		if (e.ctrlKey) {
+      if (e.key == 'ArrowUp') list.insertBefore(task, task.previousSibling)
+      if (e.key == 'ArrowDown') insertAfter(task, task.nextSibling)
+      span.focus()
+    }
   }
 })
 
-let moveCompletedTasks
-
-if ('moveCompletedTasks' in localStorage) {
-  moveCompletedTasksCheckbox = document.querySelector('input[name="moveCompletedTasks"]')
-  if (localStorage.moveCompletedTasks == 'true') {
-    moveCompletedTasks = moveCompletedTasksCheckbox.checked = true
-     true
-  } else {
-    moveCompletedTasks = moveCompletedTasksCheckbox.checked = false
-  }
-}
-
 window.addEventListener('click', e => {
-	list = document.getElementById('list')
 	tasks = list.querySelectorAll('.task')
 	lastTask = tasks[tasks.length-1]
+  if (!list.contains(e.target) && lastTask.querySelector('div > span').innerHTML !== '' && list.length !== 1) {
+    listSaver()
+  }
 	if (!lastTask.contains(e.target) && lastTask.querySelector('div > span').innerHTML == '' && lastTask !== tasks[0]) {
 		list.removeChild(lastTask)
   }
@@ -80,93 +192,19 @@ window.addEventListener('click', e => {
 		if (checkbox.checked) {
 			task.classList.toggle('completed', true)
 			span.contentEditable = false
-      if (moveCompletedTasks) insertAfter(task, lastTask)
+      if (options.moveCompletedTasks) insertAfter(task, lastTask)
 		} else {
       taskFirstCompleted = task.parentNode.querySelectorAll('div > .task.completed')[0]
 			task.classList.toggle('completed', false)
 			span.contentEditable = true
-      if (moveCompletedTasks) list.insertBefore(task, taskFirstCompleted)
+      if (options.moveCompletedTasks) list.insertBefore(task, taskFirstCompleted)
     }
 	}
 })
 
-/*
-let os
-if (navigator.appVersion.indexOf("Win") != -1) os = "Windows"
-if (navigator.appVersion.indexOf("Mac") != -1) os = "MacOS"
-if (navigator.appVersion.indexOf("X11") != -1) os = "UNIX"
-if (navigator.appVersion.indexOf("Linux") != -1) os = "Linux"
-*/
 
-// OPEN & CLOSE MODALS
-function activateModal(modal, active) {
-  modal.classList.toggle('active', active)
-}
-
-var info = document.getElementById('info')
-var infoToggle = document.getElementById('infoToggle')
-var settings = document.getElementById('settings')
-var settingsToggle = document.getElementById('settingsToggle')
-
-modals = [info, settings]
-modalsToggles = [infoToggle, settingsToggle]
-
-modalsToggles.forEach((toggle, i) => {
-  toggle.addEventListener('click', () => {
-    activateModal(modals[i], true)
-  })
-})
-
-window.addEventListener('click', e => {
-  modals.forEach(modal => {
-    if (e.target == modal && modal.classList.contains('active')) {
-  		activateModal(modal, false)
-  	}
-  })
-})
-
-// MOVE COMPLETED TASKS
-document.querySelector('input[name="moveCompletedTasks"]').addEventListener('change', e => {
-  if (e.target.checked) {
-    lastTask = tasks[tasks.length-1]
-    document.querySelectorAll('.completed').forEach(task => {
-      insertAfter(task, lastTask)
-      lastTask = task
-    })
-    moveCompletedTasks = true
-    localStorage.moveCompletedTasks = 'true'
-  } else {
-    moveCompletedTasks = false
-    localStorage.moveCompletedTasks = 'false'
-  }
-})
-
-// DARK OR LIGHT MODE
-function manageTheme(theme) {
-  if (theme == 'dark' || (theme == 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.body.classList.toggle('dark-theme', true)
-  } else {
-    document.body.classList.toggle('dark-theme', false)
-  }
-}
-
-if ('theme' in localStorage) {
-  manageTheme(localStorage.theme)
-  document.querySelector(`input[name="theme"][value="${localStorage.theme}"]`).checked = true
-}
-
-document.querySelectorAll('input[name="theme"]').forEach(radio => {
-  radio.addEventListener('change', () => {
-    manageTheme(radio.value)
-    localStorage.setItem('theme', radio.value)
-  })
-})
-
-window.matchMedia('(prefers-color-scheme: dark)').addListener(() => {
-  manageTheme(localStorage.theme)
-})
-
-// SAVE LIST
-if ('saveList' in localStorage) {
-  document.querySelector(`input[name="saveList"]`).checked = true
-}
+// let os
+// if (navigator.appVersion.indexOf("Win") != -1) os = "Windows"
+// if (navigator.appVersion.indexOf("Mac") != -1) os = "MacOS"
+// if (navigator.appVersion.indexOf("X11") != -1) os = "UNIX"
+// if (navigator.appVersion.indexOf("Linux") != -1) os = "Linux"
